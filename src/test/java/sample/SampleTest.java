d78b113362fd135e5379ecd96b2e45868ff3076f
Admin <Admin@Lenovo>
2023-09-27 16:16:47 +0530

    commit

---
+++ src/test/java/sample/SampleTest.java
@@ -0,0 +1,11 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+@Test
+public void sampleTest() {
+	System.out.println("--- Sample Test 1---");
+	System.out.println("--- Sample Test 2---");
+}
+}
