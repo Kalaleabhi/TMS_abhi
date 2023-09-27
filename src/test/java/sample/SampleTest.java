eae462a9dfebbacd8ceb1092bd5abfbe47a0451e
Admin <Admin@Lenovo>
2023-09-27 16:29:11 +0530

    Sample

--- src/test/java/sample/SampleTest.java
+++ src/test/java/sample/SampleTest.java
@@ -3,9 +3,9 @@ package sample;
 import org.testng.annotations.Test;
 
 public class SampleTest {
-@Test
-public void sampleTest() {
-	System.out.println("--- Sample Test 1---");
-	System.out.println("--- Sample Test 2---");
-}
+	@Test
+	public void sampleTest() {
+		System.out.println("--- Sample Test 1---");
+		System.out.println("--- Sample Test 2---");
+	}
 }
