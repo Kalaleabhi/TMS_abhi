ac29418d957e01f7e447581245a04c38afef7a11
Dell Inspiron 15 <Dell Inspiron 15@DESKTOP-KEFG07B>
2023-09-28 09:36:14 +0530

    New Commit

---
+++ src/test/java/adminModuleTest/ManagePagesNew01.java
@@ -0,0 +1,12 @@
+package adminModuleTest;
+
+import org.testng.annotations.Test;
+
+public class ManagePagesNew01 {
+
+	@Test
+	public void managePages01()
+	{
+		System.out.println(" From Manage Pages new");
+	}
+}
