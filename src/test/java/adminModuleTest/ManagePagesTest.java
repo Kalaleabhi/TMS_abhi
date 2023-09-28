36a47162a0c09cf902bf7845c0292b4d3a8dd3ce
Dell Inspiron 15 <Dell Inspiron 15@DESKTOP-KEFG07B>
2023-09-28 09:34:00 +0530

    Commit

--- src/test/java/adminModuleTest/ManagePagesTest.java
+++
@@ -1,8 +0,0 @@
-package adminModuleTest;
-
-public class ManagePagesTest {
-
-	public static void main(String[] args) {
-		System.out.println("From Manage Pages");
-	}
-}
--- src/test/java/adminModuleTest/ManageUsersTest.java
+++
@@ -1,8 +0,0 @@
-package adminModuleTest;
-
-public class ManageUsersTest {
-
-	public static void main(String[] args) {
-		System.out.println("From ManageUsers");
-	}
-}
